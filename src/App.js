5137bbc74b4dc44bb80c0b272db0e9d0a9fb1a44
Kim, Andrew (AssetMark) <Andrew.Kim@AssetMark.com>
2018-07-19 14:43:05 -0700

    Making changes to pass module 1 test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
-import Quiz from "./Quiz";
+import Quiz from "./Quiz.js";
 import "./App.css";
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return <Quiz />;
   }
 }
+export default App;
